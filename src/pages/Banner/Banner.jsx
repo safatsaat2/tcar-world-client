2d225e18a07674eca682f9c404fa16ac84cd0db0
safatsaat2 <sakibsafat47@gmail.com>
2023-05-19 11:44:20 +0600

    added: banner

---
+++ src/pages/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Banner = () => {
+    return (
+        <div className="hero min-h-screen w-3/4 mx-auto">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src="https://i.ibb.co/YWwDsZ4/cuate.png" className="max-w-sm rounded-lg shadow-2xl" />
+                <div className="ml-5">
+                    <h1 className="text-5xl font-bold">Where fun and learning go hand in hand</h1>
+                    <p className="py-6">The toys and games market is valued at USD 261.65 billion in the current year, and it is projected to register a CAGR of 9.91% over the next five years.</p>
+                    <Link to='/login'><button className="btn bg-[#EF8716]">Get Started</button></Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
