3aff6348c3eac99e03ba9b78a365632e1c62e525
safatsaat2 <sakibsafat47@gmail.com>
2023-05-20 00:56:25 +0600

    Added: shop by category section

--- src/pages/ShopByCategory/ShopByCategory.jsx
+++ src/pages/ShopByCategory/ShopByCategory.jsx
@@ -1,34 +1,34 @@
 import { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import ShopByCategoryCard from '../ShopByCategoryCard.jsx/ShopByCategoryCard';
 
 
 const ShopByCategory = () => {
 
     const [data, setData] = useState([])
     const [cat, setCat] = useState('truck')
-    
 
-    useEffect(()=>{
+
+    useEffect(() => {
         fetch('http://localhost:5000/category-data', {
-            method:"GET",
-            headers:{
+            method: "GET",
+            headers: {
                 "content-type": "application/json"
             }
         })
-        .then(res => res.json())
-        .then(data => setData(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, [])
 
     const filteredData = data.filter(d => d.category === cat)
-    // setData(filteredData
-    // console.log(data)
-    console.log(filteredData)
-    
+
+
 
     return (
-        <div className='w-full mx-auto'>
-            <Tabs defaultIndex={1} onSelect={(index) => index === 0 ? setCat('sports car') :  index === 1 ? setCat('truck') : setCat('suv')}>
+        <div className='w-2/3 my-20 mx-auto'>
+            <h1 className='text-4xl text-center font-bold my-10 text-[#E03C79]'>Shop by Category</h1>
+            <Tabs defaultIndex={1} onSelect={(index) => index === 0 ? setCat('sports car') : index === 1 ? setCat('truck') : setCat('suv')}>
                 <TabList>
                     <Tab > Sports Car</Tab>
                     <Tab >Truck</Tab>
@@ -36,13 +36,19 @@ const ShopByCategory = () => {
                 </TabList>
 
                 <TabPanel>
-                    <h2>Any content 1</h2>
+                    <div className='grid grid-cols-1 lg:grid-cols-2'>
+                        {filteredData.map(d => <ShopByCategoryCard key={d._id} data={d}></ShopByCategoryCard>)}
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className='grid grid-cols-1 lg:grid-cols-2'>
+                        {filteredData.map(d => <ShopByCategoryCard key={d._id} data={d}></ShopByCategoryCard>)}
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className='grid grid-cols-1 lg:grid-cols-2'>
+                        {filteredData.map(d => <ShopByCategoryCard key={d._id} data={d}></ShopByCategoryCard>)}
+                    </div>
                 </TabPanel>
             </Tabs>
         </div>
---
+++ src/pages/ShopByCategoryCard.jsx/ShopByCategoryCard.jsx
@@ -0,0 +1,25 @@
+
+import {  FaStar } from 'react-icons/fa';
+
+
+
+const ShopByCategoryCard = ({data}) => {
+    const {picture, name, price, rating} = data
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src={picture}  className="rounded-xl object-cover w-full h-48" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title text-[#EF8716]">Product Name: {name}</h2>
+                <p className='font-semibold'>Price: ${price}</p>
+                <p className='flex justify-center items-center font-semibold'> <FaStar className='mx-2 text-[#EF8716]' /> {rating}</p>
+                <div className="card-actions">
+                    <button className="btn bg-[#EF8716]">View Details</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ShopByCategoryCard;
